541a5a90bd947c7e65b9044d1dc18020c06837c0
BARATH-VIKRAMAN <41777709+BARATH-VIKRAMAN@users.noreply.github.com>
2020-11-27 08:15:26 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect('remotemysql.com', 'ddHWS8xVZr', 'yjSU7MRCpR', 'ddHWS8xVZr');
+	$con = mysqli_connect('remotemysql.com', 'ddHWS8xVZr', 'VvtUny75qK', 'ddHWS8xVZr');
 	if(!$con)
 		die("ERROR: Couldn't connect to database");
 ?>
